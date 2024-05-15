e1a4f268e1e98914a84c3abb50962985bb48f93c
teggaa <166315502+teggaa@users.noreply.github.com>
2024-05-15 10:16:19 -0700

    commit

---
+++ Day 02/Homework/level2.py
@@ -0,0 +1,18 @@
+name1 = "data tezelashvili"
+print(name1)
+name2 = "dato berkacashvili"
+print(name2)
+name3 = "dato dadiani"
+print(name3) 
+name4 = "goa"
+goa = "goa momwons radgan baton nikas kargi midgoma aqvs da kargad xsnis masalas aseve imito momwons ro yvela yvelas exmareba"
+goa1 = "chemi mtavari mizania didi fexburtis tamashi da erovnul nakrebshi moxvedra"
+print(name4)
+
+
+
+
+
+
+
+
