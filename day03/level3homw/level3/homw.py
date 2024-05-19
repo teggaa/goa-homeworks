3fe64a8adc728aaf37bb50a07e7d3ba698c3613c
teggaa <166315502+teggaa@users.noreply.github.com>
2024-05-19 10:42:52 -0700

    commit

---
+++ day03/level3homw/level3/homw.py
@@ -0,0 +1,24 @@
+print(10 + 10)
+print(10 - 10)
+print(10 * 10)
+print(10 / 10)
+
+#creating a variable
+variable1 = 10
+#creating second variable
+variable2 = 25
+#printing their sum
+print(variable1 + variable2)
+
+
+
+
+
+
+
+
+
+
+
+
+
