96d78e4d3b3485c8579cf0f41d7d48462e629365
teggaa <166315502+teggaa@users.noreply.github.com>
2024-05-28 07:06:45 -0700

    second commit

---
+++ day05/level5.py
@@ -0,0 +1,114 @@
+#creating variables
+my_name = "tega"
+my_surname = "nebulishvili"
+my_age = 15
+my_adress = "tamarashvili"
+# im creating first variables
+variable1 = 37
+#in the 8th line im creating second variable
+variable2 = 24
+#im adding them 
+print(variable1 + variable2)
+#im subtracting them
+print(variable1 - variable2)
+#im multiplying them
+print(variable1 * variable2)
+#im dividing them
+print(variable1 / variable2)
+
+#im creating third variable
+variable3 = 120
+# am creating forth variable
+variable4 = 20
+#im adding them 
+print(variable3 + variable4)
+#im subtracting them
+print(variable3 - variable4)
+#im multiplying them
+print(variable3 * variable4)
+#im dividing them
+print(variable3 / variable4)
+#am ceating 5th variable
+variable5 = 280
+# creating 6th variable
+variable6 = 40
+#im adding them 
+print(variable5 + variable6)
+#im subtracting them
+print(variable5 - variable6)
+#im multiplying them
+print(variable5 * variable6)
+#im dividing them
+print(variable5 / variable6)
+
+#ceating 7th variable
+variable7 = 14
+#creating 8th variable
+variable8 = 7
+#im adding them 
+print(variable7 + variable8)
+#im subtracting them
+print(variable7 - variable8)
+#im multiplying them
+print(variable7 * variable8)
+#im dividing them
+print(variable7 / variable8)
+
+#crating 8th variable
+variable8 = 325
+#creating 9th variable
+variable9 = 5
+#im adding them 
+print(variable8 + variable9)
+#im subtracting them
+print(variable8 - variable9)
+#im multiplying them
+print(variable8 * variable9)
+#im dividing them
+print(variable8 / variable9)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
