1b6be5d50f6375d0844cc3a0cb8d53edf5ec8c3e
teggaa <166315502+teggaa@users.noreply.github.com>
2024-05-31 11:20:34 -0700

    second commit

---
+++ day06/lv6.py
@@ -0,0 +1,29 @@
+print("please enter num1")
+#creating variable1
+num1 = int(input())
+#creating variable2
+print("please enter num2")
+num2 = int(input())
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print("please enter your name")
+name = input()
+print("congratulation tega you register")
+
+print("please enter your firstname")
+firstname = input()
+print("please enter your lastname")
+lastname = input()
+print("please enter your age")
+age = input()
+
+num1 = 40
+num2 = 45
+print(num1 + 25)
+print(num2 + 25)
+
+
+
+
