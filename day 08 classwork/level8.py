abe3b2f04c07dc2f9fd4668a755e8686a0dc2d56
teggaa <166315502+teggaa@users.noreply.github.com>
2024-06-08 07:41:30 -0700

    second commit

---
+++ day 08 classwork/level8.py
@@ -0,0 +1,6 @@
+num1 = (input("enter num1"))
+
+
+print( > 5 and 7 < 10)
+
+
---
+++ day08/level8.py
@@ -0,0 +1,110 @@
+
+# print(True and True) # True
+#and operatori aris mkacri da moitxovs orive magalitis siswores
+# print(True and False) # False
+#and operatori aris da tu erti magaliti arasworia mashin false
+# print(False and True) # False
+#erti magaliti arasworia mashin false
+# print(False and False) # False 
+#orive magaliti arasworia mashin false
+
+
+# print(True or True) # True
+#or operatori ar aris mkacri da tu erti magaliti mainc sworia mashin true
+# print(True or False) # True
+#erti magaliti sworia mashin true
+# print(False or True) # True
+#erti magaliti sworia mashin true
+# print(False or False) # False
+#orive magaliti arasworia mashin false
+
+num = 7
+print(num >= 4 and num <= 9)#true
+print(num <= 5 and num >= 6)#false
+print(num > 3 and num < 8 )#true
+print(num < 3 and num > 9)#false
+
+print(num >= 9 or num <=8)#true
+print(num <= 5 or num >= 7)#true
+print(num > 8 or num < 6)#false
+print(num > 9 or num < 8)#true
+
+num = 3
+print(num >= 2 and num <= 9)#true
+print(num <= 5 and num >= 6)#false
+print(num > 1 and num < 8 )#true
+print(num < 2 and num > 9)#false
+
+print(num >= 6 or num <=4)#true
+print(num <= 4 or num >= 7)#true
+print(num > 8 or num < 2)#false
+print(num > 6 or num < 1)#false
+
+num = 5
+
+print(num >= 2 and num <= 9)#true
+print(num <= 5 and num >= 3)#true
+print(num > 1 and num < 4 )#false
+print(num < 2 and num > 3)#false
+
+print(num >= 9 or num <= 6)#true
+print(num <= 4 or num >= 7)#false
+print(num > 8 or num < 2)#false
+print(num > 4 or num < 1)#true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
