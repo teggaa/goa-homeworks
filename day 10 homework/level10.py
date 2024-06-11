294218c1934b568e9cbe7c00405888347afdbb79
teggaa <166315502+teggaa@users.noreply.github.com>
2024-06-11 10:59:27 -0700

    second commit

---
+++ day 10 homework/level10.py
@@ -0,0 +1,33 @@
+for i in range(40):
+    print("i love georgian food")
+for i in range(60):
+    print("nikus uyvars tini")
+for i in range(10):
+    print("batoni luka mogvewona xalxs dzaan pozitiuri kacia")
+for i in range(30):
+    print("me miyvars xinkali")
+for i in range(80):
+    print("i love football, + messi and neymar")
+for i in range(10):
+    print("messi, + neymar and ronaldo are best footballers")
+for i in range(1000):
+    print("football is best sport")
+for i in range(150):
+    print("i am best footballer in my group")
+for i in range(158):
+    print("i love goa")
+for i in range():
+    print("i love my brothers")
+
+
+
+
+
+
+
+
+
+
+
+
+
